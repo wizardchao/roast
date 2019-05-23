5f0eee8b5b087444992bceeee04b2ae98ab30382
wizardchao <18317119662@139.com>
2019-05-23 09:11:54 +0800

    pack js and sass

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,30 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+// require('./bootstrap');
+window._ = require('lodash');
+try {
+    window.$ = window.jQuery = require('jquery');
+    require('foundation-sites');
+} catch (e) {}
+window.axios = require('axios');
 
-window.Vue = require('vue');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+// window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +41,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
